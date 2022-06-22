47037e171b5adc6ef2697acabd3fb89c5a0310e3
Lash Blast <evelyn.aguirre.9895@gmail.com>
2022-06-22 10:37:39 -0500

    create new files

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,24 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
-import Navbar from './components/Navbar';
+// TODO: Using ApolloClient, InMemoryCache, createHttpLink, and setContext from the Apollo Client library, create an Apollo Provider to make every request work with the Apollo server.
+
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import SearchBooks from "./pages/SearchBooks";
+import SavedBooks from "./pages/SavedBooks";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <Router>
-      <>
-        <Navbar />
-        <Switch>
-          <Route exact path='/' component={SearchBooks} />
-          <Route exact path='/saved' component={SavedBooks} />
-          <Route render={() => <h1 className='display-2'>Wrong page!</h1>} />
-        </Switch>
-      </>
-    </Router>
-  );
+	return (
+		<Router>
+			<>
+				<Navbar />
+				<Switch>
+					<Route exact path="/" component={SearchBooks} />
+					<Route exact path="/saved" component={SavedBooks} />
+					<Route render={() => <h1 className="display-2">Wrong page!</h1>} />
+				</Switch>
+			</>
+		</Router>
+	);
 }
 
 export default App;
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,53 @@
+// TODO:  Define the necessary Query and Mutation types:
+
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create typeDefs
+const typeDefs = gql`
+type Query: {
+  me: User
+}
+
+type Mutation: {
+  login: Accepts an email and password as parameters; returns an Auth type.
+  addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+  saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+  removeBook: Accepts a book's bookId as a parameter; returns a User type.}
+
+
+
+type User: {
+  _id: ID
+  username: String
+  email: String 
+  bookCountL Int
+  savedBooks: [Book]
+}
+
+
+Book type:
+
+// bookId (Not the _id, but the book's id value returned from Google's Book API.)
+
+// authors (An array of strings, as there may be more than one author.)
+
+description: String
+
+title: String
+
+image: String
+
+link: String
+
+
+Auth type:
+
+//  token
+
+//   `user` (References the `User` type.)`;
+
+// export the typeDefs
+module.exports = typeDefs;
+
+
